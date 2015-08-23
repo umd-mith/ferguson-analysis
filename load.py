4a4cbd7690249cb6160ef62f906c617713b07c9e
Ed Summers <ehs@pobox.com>
2015-08-23 21:43:33 +0000

    uncommented

--- load.py
+++ load.py
@@ -11,12 +11,11 @@ from os.path import join
 from dateutil.parser import parse as parse_date
 
 stats = redis.StrictRedis()
-#stats.flushdb()
+stats.flushdb()
 
 def analyze(data_dir):
     for tweet in tweets(data_dir):
         t = parse_date(tweet['created_at']).strftime('%Y-%m-%d')
-        """
         stats.incr('tweets-%s' % t)
 
         if 'entities' in tweet:
@@ -29,18 +28,14 @@ def analyze(data_dir):
         if 'entities' in tweet and 'media' in tweet['entities']:
             for media in tweet['entities']['media']:
                 stats.zincrby('media-%s' % t, media['media_url'], 1)
-        """
 
         if 'entities' in tweet and 'urls' in tweet['entities']:
             for url in tweet['entities']['urls']:
                 stats.zincrby('urls-%s' % t, url['expanded_url'], 1)
 
-        """
-
         if 'retweeted_status' in tweet:
             stats.zincrby('retweets-%s' % t, tweet['retweeted_status']['id_str'], 1)
             stats.zincrby('users-%s' % t, tweet['retweeted_status']['user']['screen_name'], 1)
-        """
 
 def tweets(data_dir):
     count = 0
