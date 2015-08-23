90d1507203db4ea2dd86b35bd5482d56faf86efe
Ed Summers <ehs@pobox.com>
2015-08-23 17:20:41 -0400

    consolidated

---
+++ report.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import redis
+
+stats = redis.StrictRedis()
+times = [s.split('-', 1)[1] for s in stats.keys('tweets-*')]
+times.sort()
+
+def main():
+    tweets()
+    retweets()
+    users()
+    hashtags()
+    media()
+    urls()
+
+def hashtags():
+    print "## Hashtags"
+    for time in times:
+        print
+        print time
+        for tag in stats.zrevrange('hashtags-%s' % time, 0, 10, withscores=True):
+            print '- %s (%i)' % tag
+
+def media():
+    print "## Media"
+    for time in times:
+        print
+        print time
+        for tag in stats.zrevrange('media-%s' % time, 0, 5, withscores=True):
+            print '- %s (%i)' % tag
+
+def retweets():
+    print "## Retweets"
+    for time in times:
+        print
+        print time
+        for tag in stats.zrevrange('retweets-%s' % time, 0, 10, withscores=True):
+            print '- %s (%i)' % tag
+
+def tweets():
+    print "## Tweets"
+    total = 0
+    for time in times:
+        num = int(stats.get('tweets-%s' % time))
+        print time, num
+        total += num
+    print "total: %s" % total
+
+def urls():
+    print "## URLs"
+    for time in times:
+        print
+        print time
+        for tag in stats.zrevrange('urls-%s' % time, 0, 10, withscores=True):
+            print '- %s (%i)' % tag
+
+def users():
+    print "## Users"
+    for time in times:
+        print
+        print time
+        for tag in stats.zrevrange('users-%s' % time, 0, 10, withscores=True):
+            print '- %s (%i)' % tag
+
+if __name__ == "__main__":
+    main()
